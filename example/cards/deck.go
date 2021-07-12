4b6885791e7d9d8ca21179c68ad698ecff5d8d5e
Chalermchai Viriyamanatham <Chalermchai_v@kkumail.com>
2021-07-12 22:19:22 +0700

    Add function to read file contain cards

--- example/cards/deck.go
+++ example/cards/deck.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"io/ioutil"
+	"os"
 	"strings"
 )
 
@@ -47,6 +48,19 @@ func (d deck) toString() string {
 	return strings.Join([]string(d), ",")
 }
 
-func (d deck) saveToFile(filename string) error{
+func (d deck) saveToFile(filename string) error {
+	// Use WriteFile() to write deck of cards to text file
+	// permission 0666 mean anyone can read and write
 	return ioutil.WriteFile(filename,[]byte(d.toString()), 0666)
+}
+
+func newDeckFromFile(filename string) deck {
+	bs, err := ioutil.ReadFile(filename)
+	if err != nil {	
+		fmt.Println("Error:", err)
+		os.Exit(1)
+	}
+
+	s := strings.Split(string(bs), ",")
+	return deck(s)
 }
--- example/cards/main.go
+++ example/cards/main.go
@@ -1,19 +1,22 @@
 package main
 
-import "fmt"
+// import "fmt"
 
 func main() {
 	// Initailizer value for deck of cards
 	// By call newDeck(). It's return list of cards 
-	cards := newDeck()
+	// cards := newDeck()
 
 	// hand: Player will call cards by deal with number of card
 	// remainingCards: Remaining cards in the deck
-	hand, remainingCards := deal(cards, 5)
+	// hand, remainingCards := deal(cards, 5)
 	
-	hand.print()
-	remainingCards.print()
+	// hand.print()
+	// remainingCards.print()
 
-	fmt.Println(cards.toString())
-	cards.saveToFile("my_cads")
+	// fmt.Println(cards.toString())
+	// cards.saveToFile("my_cards")
+
+	cards := newDeckFromFile("my_cards")
+	cards.print()
 }
